ea0fb837317f81d14d9c16c7f2b05e4a05fa2003
Camden Brown <camdenebrown@gmail.com>
2017-08-25 09:13:59 -0500

    change recipe name

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -11,7 +11,7 @@ export class RecipeListComponent implements OnInit {
   @Output() recipeWasSelected = new EventEmitter<Recipe>();
   recipes: Recipe[] = [
     new Recipe('Chicken Pot Pie', 'A very delicious pie', `http://food.fnr.sndimg.com/content/dam/images/food/fullset/2009/6/12/0/IG0604H_33099_s4x3.jpg.rend.hgtvcom.616.462.suffix/1384540893720.jpeg`),
-    new Recipe('Chicken Pot Pie', 'A very delicious pie', `http://food.fnr.sndimg.com/content/dam/images/food/fullset/2009/6/12/0/IG0604H_33099_s4x3.jpg.rend.hgtvcom.616.462.suffix/1384540893720.jpeg`)
+    new Recipe('Beef Pie', 'A very delicious pie', `http://food.fnr.sndimg.com/content/dam/images/food/fullset/2009/6/12/0/IG0604H_33099_s4x3.jpg.rend.hgtvcom.616.462.suffix/1384540893720.jpeg`)
   ];
 
   constructor() { }
